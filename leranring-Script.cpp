eea7a0ced354240872f79a9dfe6c95aa132048fe
rwx777 <joshuajared007@gmail.com>
2019-10-23 19:55:29 +0200

    Learning_Script.cpp
    Added 2D Arrays, Pointers, Classes & Objects.

--- leranring-Script.cpp
+++ leranring-Script.cpp
@@ -1,4 +1,4 @@
-//2019
+//2019 C++ Learning Repo
 //Simple C++ Learning Script
 //written by rwx-777
 
@@ -6,6 +6,18 @@
 #include <cmath> //math functions
 
 using namespace std;
+
+//Classes
+
+class Book { //A Class is the template/specification of a data type
+    //We can give a class attributes 
+    public:
+        string title;
+        string author;
+        int pages;
+};
+//An object is an actual instance of that class
+
 
 //Functions
 
@@ -42,13 +54,14 @@ int getMax(int num1, int num2) { //Function to get the biggest Number that was i
 
     }
 
+//switch statememt
 //Function to convert ints to strings using day names
     string getDayOfWeek(int dayNum) {
         string dayName;
 
         switch(dayNum) {
-            case 0:
-                dayName = "Sunday";
+            case 0: //in the case that dayNum is 0 it will execute the code in Curly brackets
+                dayName = "Sunday"; 
                 break;
             case 1: //in the case that dayNum = 1 it will assign Monday to dayName
                 dayName = "Monday";
@@ -68,9 +81,22 @@ int getMax(int num1, int num2) { //Function to get the biggest Number that was i
             case 6:
                 dayName = "Saturday";
                 break;
+            default: //if none of the cases above are true the code in default will be executed 
+            //similar to if else and default is else
+                dayName = "Invalid Day Number.\nThere are only 7 days in a week...";
         }
 
     }
+
+    //Exponent Function
+    int power(int baseNum, int powNum) {
+        int result_of_power = 1;
+        for (int i = 0; i < powNum; i++){
+            result_of_power = result_of_power * baseNum;
+        }
+        
+    }
+
 
 
 int main() {
@@ -239,9 +265,151 @@ int main() {
 
     //Switch Statements
     // A switch statements saves us from makeing lots of different else if Statements
+    //Check the function on line 45 to see a Switch Statement
 
+    cout << getDayOfWeek(0); //prints out "Sunday"
+    newline();
+    cout << getDayOfWeek(1); //prints out "Monday"
+    newline();
+    cout << getDayOfWeek(777); //prints out "Invalid Day Number"
+    newline();
+
+    //##############################################################################################################
+    
+    //While Loops
+
+    int index = 1;
+    //if index were to be 6 the code would never execute
+
+    while(index <= 5) { //Condition code will be executed as long as the conditon is true
+        cout << index << endl;
+        index++; //adds 1 to index
+    } //Will print out 1 to 5 
+    //Watch out for infinite Loops ! Could cause Crashes
+
+
+    //Do While Loop
+    //its like a reversed While loop
+    //First the code gets executed then the condition gets checked
+
+    int index2 = 1;
+
+    do{
+        cout << index2 << endl;
+        index2++;
+    }while (index2 <= 5);
+
+    //#################################################################################################################
+
+    // For Loops
+
+    for(int i = 1; i <= 5 ;i++){
+        cout << i << endl; 
+    }
+
+    //Lets do something a little more advanced
+    //using a for loop to iterate through an Array of Ints
+
+    int nums[] = {1,2,4,5,7,3};
+    for(int i = 0; i < 6; i++){
+        cout << nums[i] << endl;
+            //Will print out everything until nums[5]
+    }
+
+
+    //########################################################################################################
+
+    //Exponent Function
+    //Will take a number and take it to the power of a number
+    //See function in line 79
+    
+    cout << power(2, 3); //prints out 8
+    newline();
+    cout << power(2,4);
+    newline();
+
+    //#########################################################################################################
+
+    //2D Array
+    //A 2D Array is an Array where every Element of the Array is another Array
+
+    int numberGrid[3][2] = { //How many elements are in the Array / how many elements are in the Arrays Array 
+                            {1, 2}, //this is the first element of the Array Number Grid and its a Array
+                            {3, 4}, // this is the second element of the Array Number Grid and its a Array
+                            {5, 6}
+                        };
+    
+    cout << numberGrid[0][1]; //prints out 2
+    newline();
+    cout << numberGrid[2][1]; //prints out 6
+    newline();
+
+    //Nested For Loop
+
+    for(int i = 0; i < 3; i++){
+        for (int j = 0; j < 2; i++){
+
+            cout << numberGrid[i][j];
+        }
+        cout << endl;
+        
+    }
+
+    //#######################################################################################################
+
+    //Pointers
+    //Did you guys know all of these values get stored inside the RAM in a physical memory address
+    //Lets try to access it
+    int age4 = 19;
+    cout << &age4; //prints out the physical memory address where 19 is stored
+    //Aka we could say we are printing out a pointer (memory address)
+
+    double iq = 97.7;
+    cout << "IQ: " << &iq << endl;
+    newline();
+
+    string NickName = "Zack";
+
+    cout << "Nick Name: " << &NickName << endl;
+
+    //Lets store the Pointers with a Pointer variable to store a memory address
+
+    int *pAge = &age4; //will store the pointer in this variable
+
+    string *pNickName = &NickName;
+
+    newline();
+    cout << pAge; //Prints out the Pointer of age4
+
+    //How to dereference a Pointer to get the value of the memory address
+
+    cout << *pAge; //Pointer is now dereferenced
+    //Will print out value 19
 
     
+    //#############################################################################################
+
+    //Classes & Objects
+
+    //New Class created on line 11
+
+    Book book1;
+    book1.title = "The Great Gatsby";
+    book1.author = "F. Scott Fitzgerald";
+    book1.pages = 512;
+
+    cout << book1.title << endl;
+
+    Book book2;
+    book2.title = "The Lord of the Rings";
+    book2.author = "J.R. Tolkein";
+    book2.pages = 700;
+
+    cout << book2.title << endl;
+
+    
+
+
 
 
 
